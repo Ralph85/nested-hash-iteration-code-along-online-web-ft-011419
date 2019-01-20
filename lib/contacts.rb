7c485b3a9f5b055f28665ca7be61bd47f7f588b3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-20 21:48:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ contacts = {
 }
 
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attrribute, data|
+    contact_details_hash.each do |attribute, data|
       binding.pry
     end
 end
